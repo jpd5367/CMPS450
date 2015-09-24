ef88fa47749aafd003a80c7017d0b5e78f686627
Joe DeHart <lilgsrsoldier@gmail.com>
2015-09-24 03:26:07 -0500

    removed project test case

--- testCases.scm
+++ testCases.scm
@@ -4,6 +4,28 @@
 	                12 13 14 15 16 17 18 19 20))
 (define testData4 '(5 4 3 2 1 0 -1 -2 -3 -4 -5))
 (define testData5 '(1 2 3))
+(define sorted1   '(-34 1 2 4 8 45 345))
+(define sorted2   '(-23 4 434 700 60000 234234))
+(define sorted3   testData3)
+(define sorted4   testData4)
+(define sorted5   testData5)
+
+(define isListSorted
+	(lambda (l)
+		(if (null? l)
+			#t
+			(if (null? (cdr l))
+				#t
+				(if (> (car l) (cadr l))
+					#f
+					(
+						isListSorted (cdr l)
+					)
+				)
+			)
+		)
+	)
+)
 
 (define testCount
 	(lambda ()
@@ -35,39 +57,99 @@
 (define testMinMax
 	(lambda ()
 		(
-			'()
-		)
-	)
-)
-
-(define testUserInts
-	(lambda ()
-		(
-			'()
-		)
-	)
-)
-
-(define testUserMinMax
-	(lambda ()
-		(
-			'()
+			and
+			(= ; test min of data 1 
+				(car (list-min-max testData1))
+				-34
+			)
+			(= ; test max of data 1
+				(cdr (list-min-max testData1))
+				345
+			)
+			(= ; test min of data 2 
+				(car (list-min-max testData2))
+				-23
+			)
+			(= ; test max of data 2
+				(cdr (list-min-max testData2))
+				234234
+			)
+			(= ; test min of data 3 
+				(car (list-min-max testData3))
+				1
+			)
+			(= ; test max of data 3
+				(cdr (list-min-max testData3))
+				20
+			)
+			(= ; test min of data 4 
+				(car (list-min-max testData4))
+				-5
+			)
+			(= ; test max of data 4
+				(cdr (list-min-max testData4))
+				5
+			)
+			(= ; test min of data 5 
+				(car (list-min-max testData5))
+				1
+			)
+			(= ; test max of data 5
+				(cdr (list-min-max testData5))
+				3
+			)
 		)
 	)
 )
 
 (define testMergeSort
 	(lambda ()
-		(
-			'()
+		(and 
+			(isListSorted (merge-sort testData1))
+			(isListSorted (merge-sort testData2))
+			(isListSorted (merge-sort testData3))
+			(isListSorted (merge-sort testData4))
+			(isListSorted (merge-sort testData5))
 		)
 	)
 )
 
 (define testHigherOrder
 	(lambda ()
-		(
-			'()
+		(and
+			(= 
+				((inc_n 2) 3)
+				5
+			)
+			(= 
+				((inc_n 55) 25)
+				80
+			)
+			(= 
+				((inc_n -2) 3)
+				1
+			)
+			(= 
+				((inc_n 128) 4)
+				132
+			)
+			(= 
+				((inc_n 24) 30)
+				54
+			)
+			(= 
+				((inc_n 1) 3)
+				4
+			)
+			(= 
+				((inc_n 100000) 3000)
+				103000
+			)
+			(= 
+				((inc_n -200) 3)
+				-197
+			)
+
 		)
 	)
 )
